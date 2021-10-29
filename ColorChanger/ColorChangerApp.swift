5e440b5210660e4c0c6fa2c6024de51cb11dcf4a
Nadzeya Shpakouskaya <shpakovna@gmail.com>
2021-10-29 10:51:39 +0400

    Initial Commit

---
+++ ColorChanger/ColorChangerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorChangerApp.swift
+//  ColorChanger
+//
+//  Created by Nadzeya Shpakouskaya on 29/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct ColorChangerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
